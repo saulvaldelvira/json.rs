1e97200f2045cc73222cb459bedfd692c390503e
Saúl Valdelvira <saul@saulv.es>
2025-05-19 11:52:40 +0200

    Switch to GPL license

--- build.rs
+++ build.rs
@@ -11,6 +11,7 @@ fn main() {
     cbindgen::Builder::new()
         .with_crate(crate_dir)
         .with_language(cbindgen::Language::C)
+        .with_header(GPL_HEADER)
         .generate()
         .map_or_else(
             |error| match error {
@@ -21,4 +22,19 @@ fn main() {
                 bindings.write_to_file("target/include/json.h");
             },
         );
+
+    const GPL_HEADER: &str = "\
+    /*  Copyright (C) 2025 Saúl Valdelvira
+     *
+     *  This program is free software: you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation, version 3.
+     *
+     *  This program is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  You should have received a copy of the GNU General Public License
+     *  along with this program.  If not, see <https://www.gnu.org/licenses/>. */";
 }
